5037fd10a45bd765b4d8207c2a7025bd45bee917
KSKarthik18 <kaknurkarthik@gmail.com>
2025-03-11 16:06:32 +0530

    message

--- Selenium_maven_practice/src/test/java/gitbash/Gitbash.java
+++ Selenium_maven_practice/src/test/java/gitbash/Gitbash.java
@@ -5,7 +5,7 @@ public class Gitbash {
 	public static void main(String[] args) {
 		System.out.println("Hello Selenium");
 		System.out.println("hi Luffy");
-
+		System.out.println("hi rummy");
 	}
 
 }
